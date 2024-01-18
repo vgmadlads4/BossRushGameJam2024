0a9fc9ed68b355a075bfc189822a319f43ecd54d
vgmadlads4 <vgmadlads@gmail.com>
2024-01-17 19:12:08 -0500

    Updates

--- Monopoly Powertrip/Assets/Scripts/bossFight.cs
+++ Monopoly Powertrip/Assets/Scripts/bossFight.cs
@@ -6,6 +6,7 @@ public class bossFight : MonoBehaviour
 {
     public int bossHealth = 9;
     public int bossDirection = 1;
+    public GameObject bossBullet;
 
     // Start is called before the first frame update
     void Start()
@@ -36,31 +37,47 @@ public class bossFight : MonoBehaviour
             }
             else if (bossHealth <= 6 && bossHealth > 3)
             {
-                this.GetComponent<Transform>().position = this.GetComponent<Transform>().position;
-                yield return new WaitForSeconds(1);
+                this.GetComponent<Transform>().Translate(Vector2.zero);
+                this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 this.GetComponent<Rigidbody2D>().gravityScale = 0;
+                //yield return new WaitForSeconds(1);
                 int randomPlace = Random.Range(0, 4);
                 switch (randomPlace)
                 {
                     case 0:
                         this.GetComponent<Transform>().position = GameObject.Find("topLeft").GetComponent<Transform>().position;
-                        yield return new WaitForSeconds(10);
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+                        StartCoroutine(bulletsFlyingOff());
+                        yield return new WaitForSeconds(2);
                         break;
                     case 1:
                         this.GetComponent<Transform>().position = GameObject.Find("topRight").GetComponent<Transform>().position;
-                        yield return new WaitForSeconds(10);
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+                        StartCoroutine(bulletsFlyingOff());
+                        yield return new WaitForSeconds(2);
                         break;
                     case 2:
                         this.GetComponent<Transform>().position = GameObject.Find("bottomLeft").GetComponent<Transform>().position;
-                        yield return new WaitForSeconds(10);
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+                        StartCoroutine(bulletsFlyingOff());
+                        yield return new WaitForSeconds(2);
                         break;
                     case 3:
                         this.GetComponent<Transform>().position = GameObject.Find("bottomRight").GetComponent<Transform>().position;
-                        yield return new WaitForSeconds(10);
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+                        StartCoroutine(bulletsFlyingOff());
+                        yield return new WaitForSeconds(2);
                         break;
                     default:
                         this.GetComponent<Transform>().position = GameObject.Find("topLeft").GetComponent<Transform>().position;
-                        yield return new WaitForSeconds(10);
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+                        StartCoroutine(bulletsFlyingOff());
+                        yield return new WaitForSeconds(2);
                         break;
                 }
             }
@@ -90,6 +107,14 @@ public class bossFight : MonoBehaviour
     public void bossHealthDecrease()
     {
         bossHealth--;
+    }
+
+    IEnumerator bulletsFlyingOff()
+    {
+        GameObject theBullet = Instantiate(bossBullet);
+        bossBullet.GetComponent<Rigidbody2D>().velocity = Vector2.right * 10;//(this.GetComponent<Transform>().position - GameObject.FindWithTag("Player").GetComponent<Transform>().position);
+        yield return new WaitForSeconds(2);
+        Destroy(theBullet);
     }
 
 }
