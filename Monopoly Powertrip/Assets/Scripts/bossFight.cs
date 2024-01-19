66a9785439786b1dbe9d4f6b5fdfce0b3b22a78d
vgmadlads4 <vgmadlads@gmail.com>
2024-01-18 21:56:47 -0500

    More Changes

--- Monopoly Powertrip/Assets/Scripts/bossFight.cs
+++ Monopoly Powertrip/Assets/Scripts/bossFight.cs
@@ -42,7 +42,7 @@ public class bossFight : MonoBehaviour
                 this.GetComponent<Rigidbody2D>().gravityScale = 0;
                 this.GetComponent<Rigidbody2D>().isKinematic = true;
                 //yield return new WaitForSeconds(1);
-                int randomPlace = Random.Range(0, 4);
+                int randomPlace = 2;//Random.Range(0, 4);
                 switch (randomPlace)
                 {
                     case 0:
@@ -112,17 +112,11 @@ public class bossFight : MonoBehaviour
 
     IEnumerator bulletsFlyingOff()
     {
-        GameObject theBullet = Instantiate(bossBullet,this.GetComponent<Transform>().position, this.GetComponent<Transform>().rotation);
-        //theBullet.GetComponent<Transform>().transform.LookAt(GameObject.FindWithTag("Player").GetComponent<Transform>().transform.position);
-        theBullet.GetComponent<Rigidbody2D>().velocity = 15*Vector2.MoveTowards(theBullet.GetComponent<Transform>().position,GameObject.FindWithTag("Player").GetComponent<Transform>().transform.position,1f);
-            
-            
-            
-         
-        //print(playerPosition);
-        //GameObject.FindWithTag("Player").GetComponent<Transform>().position.
-        yield return new WaitForSeconds(2);
-        Destroy(theBullet);
+        Vector3 bulletRotation = Quaternion.FromToRotation(GameObject.Find("forwardBullet").GetComponent<Transform>().position, GameObject.FindWithTag("Player").GetComponent<Transform>().transform.position).eulerAngles;
+        GameObject.Find("theBossBarrell").transform.rotation = Quaternion.Euler(bulletRotation);
+        //theBullet.GetComponent<Rigidbody2D>().velocity =
+        //print(theBullet.GetComponent<Rigidbody2D>().velocity);
+        yield return null;
     }
 
 }
