8d9e84a5e9b7ca7ed65a4a8766309b66112baff2
vgmadlads4 <vgmadlads@gmail.com>
2024-01-21 20:20:56 -0500

    Changes

---
+++ Monopoly Powertrip/Assets/Scripts/bossBulletHit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossBulletHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(),GameObject.FindWithTag("Boss").GetComponent<Collider2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap")
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            //GameObject.FindWithTag("Boss").SendMessage("bossHealthDecrease");
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Monopoly Powertrip/Assets/Scripts/bossFight.cs
+++ Monopoly Powertrip/Assets/Scripts/bossFight.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class bossFight : MonoBehaviour
 {
@@ -18,6 +19,19 @@ public class bossFight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (bossHealth <= 6 && bossHealth > 3)
+        {
+            GameObject.Find("centerOfChamber").GetComponent<Transform>().LookAt(GameObject.Find("playerCenter").GetComponent<Transform>().position);
+            Vector3 theZFactor = GameObject.Find("centerOfChamber").GetComponent<Transform>().rotation.eulerAngles;
+            if (theZFactor.y == 270)
+            {
+                GameObject.Find("theBossBarrell").GetComponent<Transform>().rotation = Quaternion.Euler(0f, 0f, theZFactor.x);
+            }
+            if (theZFactor.y == 90)
+            {
+                GameObject.Find("theBossBarrell").GetComponent<Transform>().rotation = Quaternion.Euler(0f, 0f, 180 - theZFactor.x);
+            }
+        }
 
     }
 
@@ -42,32 +56,25 @@ public class bossFight : MonoBehaviour
                 this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 this.GetComponent<Rigidbody2D>().gravityScale = 0;
                 this.GetComponent<Rigidbody2D>().isKinematic = true;
-                //yield return new WaitForSeconds(1);
-                int randomPlace = 3;//Random.Range(0, 4);
+                yield return new WaitForSeconds(1);
+                int randomPlace = Random.Range(0, 3);
                 switch (randomPlace)
                 {
                     case 0:
-                        this.GetComponent<Transform>().position = GameObject.Find("topLeft").GetComponent<Transform>().position;
+                        this.GetComponent<Transform>().position = GameObject.Find("top").GetComponent<Transform>().position;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
                         StartCoroutine(bulletsFlyingOff());
                         yield return new WaitForSeconds(2);
                         break;
                     case 1:
-                        this.GetComponent<Transform>().position = GameObject.Find("topRight").GetComponent<Transform>().position;
-                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
-                        this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
-                        StartCoroutine(bulletsFlyingOff());
-                        yield return new WaitForSeconds(2);
-                        break;
-                    case 2:
                         this.GetComponent<Transform>().position = GameObject.Find("bottomLeft").GetComponent<Transform>().position;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
                         StartCoroutine(bulletsFlyingOff());
                         yield return new WaitForSeconds(2);
                         break;
-                    case 3:
+                    case 2:
                         this.GetComponent<Transform>().position = GameObject.Find("bottomRight").GetComponent<Transform>().position;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
@@ -75,7 +82,7 @@ public class bossFight : MonoBehaviour
                         yield return new WaitForSeconds(2);
                         break;
                     default:
-                        this.GetComponent<Transform>().position = GameObject.Find("topLeft").GetComponent<Transform>().position;
+                        this.GetComponent<Transform>().position = GameObject.Find("top").GetComponent<Transform>().position;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
                         this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
                         StartCoroutine(bulletsFlyingOff());
@@ -83,16 +90,27 @@ public class bossFight : MonoBehaviour
                         break;
                 }
             }
+            else if (bossHealth <=3 && bossHealth >0)
+            {
+                if (bossDirection == 1)
+                {
+                    this.GetComponent<Transform>().Translate(Vector2.left * 0.01f);
+                }
+                if (bossDirection == 2)
+                {
+                    this.GetComponent<Transform>().Translate(Vector2.right * 0.01f);
+                }
+                StartCoroutine(randomBulletTime());
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+
             yield return null; ;
         }
         yield return new WaitForSeconds(2);
     }
-
-    void randomBulletTime()
-    {
-
-    }
-
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -113,21 +131,49 @@ public class bossFight : MonoBehaviour
 
     IEnumerator bulletsFlyingOff()
     {
-        GameObject.Find("centerOfChamber").GetComponent<Transform>().LookAt(GameObject.Find("playerCenter").GetComponent<Transform>().position);
-        Vector3 theZFactor = GameObject.Find("centerOfChamber").GetComponent<Transform>().rotation.eulerAngles;
-        if(theZFactor.y == 270)
+        GameObject theBullet = Instantiate(bossBullet, GameObject.Find("forwardBullet").transform.position, GameObject.Find("forwardBullet").transform.rotation);
+        theBullet.GetComponent<Rigidbody2D>().velocity = 10f*((GameObject.Find("playerCenter").GetComponent<Transform>().position - GameObject.Find("forwardBullet").transform.position).normalized);
+        yield return new WaitForSeconds(2);
+        Destroy(theBullet);
+        yield return null;
+    }
+
+    IEnumerator randomBulletTime()
+    {
+        if (GameObject.FindGameObjectsWithTag("BossBullet").Length <= 6)
         {
-            GameObject.Find("theBossBarrell").GetComponent<Transform>().rotation = Quaternion.Euler(0f, 0f, theZFactor.x);
+            int randomGun = Random.Range(0, 3);
+            switch (randomGun)
+            {
+                case 0:
+                    GameObject theBullet = Instantiate(bossBullet, GameObject.Find("topLeft").transform.position, GameObject.Find("topLeft").transform.rotation);
+                    theBullet.GetComponent<Rigidbody2D>().velocity = 10f * Vector2.down;
+                    yield return new WaitForSeconds(2);
+                    Destroy(theBullet);
+                    yield return null;
+                    break;
+                case 1:
+                    GameObject theBulletTwo = Instantiate(bossBullet, GameObject.Find("topMiddle").transform.position, GameObject.Find("topMiddle").transform.rotation);
+                    theBulletTwo.GetComponent<Rigidbody2D>().velocity = 10f * Vector2.down;
+                    yield return new WaitForSeconds(2);
+                    Destroy(theBulletTwo);
+                    yield return null;
+                    break;
+                case 2:
+                    GameObject theBulletThree = Instantiate(bossBullet, GameObject.Find("topRight").transform.position, GameObject.Find("topRight").transform.rotation);
+                    theBulletThree.GetComponent<Rigidbody2D>().velocity = 10f * Vector2.down;
+                    yield return new WaitForSeconds(2);
+                    Destroy(theBulletThree);
+                    break;
+                default:
+                    GameObject theBulletFour = Instantiate(bossBullet, GameObject.Find("topMiddle").transform.position, GameObject.Find("topMiddle").transform.rotation);
+                    theBulletFour.GetComponent<Rigidbody2D>().velocity = 10f * Vector2.down;
+                    yield return new WaitForSeconds(2);
+                    Destroy(theBulletFour);
+                    break;
+            }
         }
 
-
-
-        //GameObject theBullet = Instantiate(bossBullet, GameObject.Find("forwardBullet").transform.position, GameObject.Find("forwardBullet").transform.rotation);
-        //theBullet.GetComponent<Rigidbody2D>().velocity = new Vector3()
-        //yield return new WaitForSeconds(2);
-        //Destroy(theBullet);
-
-        yield return null;
     }
 
 }
